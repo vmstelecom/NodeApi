aafc08fb0fd21f01e18bb41f39258a49147ed04c
Vinicius Moreira Santos <vmstelecom@gmail.com>
2023-07-24 16:28:30 -0300

    terceiro commit

--- src/controllers/CarroController.js
+++ src/controllers/CarroController.js
@@ -1,1 +1,25 @@
 const CarroService = require('../services/CarroService.js');
+
+module.exports = {
+    buscarTodos: async (req, res) => {
+        let json = {error: '', result: []};
+        let carros = await CarroService.buscarTodos();
+
+        for (let i in carros) {
+            json.result.push({
+                id: carros[i].id,
+                dcad: carros[i].dcad,
+                modelo: carros[i].modelo,
+                marca: carros[i].marca,
+                placa: carros[i].placa,
+                km: carros[i].km,
+                data: carros[i].data,
+                valor_litro: carros[i].valor_litro,
+                valor_total: carros[i].valor_total,
+                litros: carros[i].litros,
+                consumo: carros[i].consumo
+            });          
+        }
+        return res.json(json);
+    }
+}
--- src/services/CarroService.js
+++ src/services/CarroService.js
@@ -1,5 +1,12 @@
 const db = require('../db.js');
 
 module.exports = {
-
+    buscarTodos: () => {
+        return new Promise((aceito, rejeitado) => {
+            db.query('SELECT * FROM carros', (error, results) => {
+                if(error) { rejeitado(error); return;}
+                aceito(results);
+            });
+        });
+    }
 };
